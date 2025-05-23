c03a8ae30e811f785f369b02452a013d25b43656
Karl Santiago Bernaldez <bernaldezkarlsantiago@gmail.com>
2025-05-23 01:24:24 +0000

    fix(jest): jest config fix for workflow

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,11 @@
 /** @type {import('jest').Config} **/
 module.exports = {
-  preset: 'ts-jest',             // Use ts-jest to handle TypeScript files
-  testEnvironment: 'node',       // Set the test environment to Node.js
+  preset: 'ts-jest',   // Use ts-jest to handle TypeScript files
+  testEnvironment: 'node',
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',  // Transform TypeScript files using ts-jest
   },
+  transformIgnorePatterns: [
+    '/node_modules/(?!ts-jest)',   // Ensure ts-jest files are transformed even if they are in node_modules
+  ],
 };
